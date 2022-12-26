cc6ee92be189273f3209024870e41519763d6aa0
tjuric03 <tonij.juric@hotmail.com>
2022-12-26 22:38:16 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+package $package;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
