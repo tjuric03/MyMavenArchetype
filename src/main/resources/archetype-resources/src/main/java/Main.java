1e483f16022033afe4ee7c5c39e79ec7d28c4e3d
tjuric03 <tonij.juric@hotmail.com>
2022-12-28 10:08:36 +0100

    Fixing plugins & adding log4j2 support

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -1,9 +1,14 @@
 package $package;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main
 {
+    public static final Logger Log = LogManager.getLogger();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Log.info("Testing the logger");
     }
 }
